7d1d5de0afb480b83f832dbdde0484e171118d77
Leo <leo.anyanwu@zkteco-wa.com>
2021-03-31 16:58:55 +0100

    fix issues

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const token = localStorage.getItem('maono_token');
 
 const http = axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
+  baseURL: process.env.VUE_APP_API_URL || 'http://35.193.7.83:8080/api/v1/',
   timeout: 7199,
   headers: {
     'Authorization': 'Bearer ' + token,
